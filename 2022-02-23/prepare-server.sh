8eb5a3e292f53f0e2d9c9e6f57862350c494eafa
Tomáš Kozák <kozak@talko.cz>
2022-02-20 18:16:31 +0100

    Add comment to prepare server script

--- 2022-02-23/prepare-server.sh
+++ 2022-02-23/prepare-server.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/bash
 
+# DO NOT USE FOR PRODUCTION ENVIRONMENT
 # script prepare environment for docker-compose
 # tested on Ubuntu 20.04
 
 # replace debian mirror - DO seems to be useless
 cp sources.list /etc/apt/sources.list
 
+# install dependencies
 apt-get update
 apt-get upgrade -y
 apt-get install -y \
@@ -20,14 +22,17 @@ apt-get install -y \
   libfcgi0ldbl \
   whois
 
+# install docker engine and docker-compose
 curl -fsSL https://get.docker.com -o get-docker.sh
 chmod +x get-docker.sh
 ./get-docker.sh
 apt install -y docker-compose
 
+# make vim beautiful
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 
+# install ohmyzsh and do NOT run it immediately
 RUNZSH=no sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # aliases
